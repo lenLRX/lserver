02470ea587651eef04dc06d3a816238170d605d4
liruixin <545976176@qq.com>
2016-09-28 16:43:28 +0800

    finish http 304

--- server/RequestHandler.cpp
+++ server/RequestHandler.cpp
@@ -3,11 +3,9 @@
 #include <time.h>
 
 #include "../log/log.h"
-#include "../network/HttpResponse.h"
 #include "../utility/html_reader.h"
 #include "../utility/resource_reader.h"
-#include "../utility/LastModifiedTime.h"
-#include "../utility/ctime2rfc822date.h"
+#include "../utility/TimeUtility.h"
 #include "vpath.h"
 
 RequestHandler::RequestHandler(){
@@ -21,6 +19,8 @@ RequestHandler::~RequestHandler(){
 void RequestHandler::handle(Connection conn,HttpRequest request){
 	string path = request.uri;
 
+	LOG << "request.uri: " << path << endl;
+
 	if(request.uri == "/")
 	    path = "/web/hello.html";
 
@@ -31,22 +31,18 @@ void RequestHandler::handle(Connection conn,HttpRequest request){
 		time_t mtime = LastModifiedTIme(path);
 		string originTime(ctime(&mtime));
 		string rfctime(ctime2rfc822date(originTime));
-		LOG << rfctime << endl;
-		response.StatusCode = 200;
-		response.addField(string("Cache-Control:public"));
-		response.addField(string("Last-Modified:") + rfctime);
+		if(request.headers.find("If-Modified-Since") != 
+		request.headers.end() && rfc822date2time_t(rfctime) == mtime){
+			code304(response);
+		}else{
+			code200(response,rfctime,path);
+		}
 	}else{
-		response.StatusCode = 404;
-		path = _vpath.translate("/web/404.html");;
+		path = _vpath.translate("/web/404.html");
+		code404(response,path);
 	}
 
 	LOG << "response.StatusCode = " << to_string(response.StatusCode) << endl;
-
-	
-
-	Resource resource = ResourceReader::getResource(path);
-	response.setContent(resource.getResouce());
-	response.setContentType(resource.getType());
 
 	ByteBuffer byteBuffer = response.getBuffer();
 		
@@ -55,4 +51,26 @@ void RequestHandler::handle(Connection conn,HttpRequest request){
 		conn.write(ret.first,ret.second);
 	}
 	
+}
+
+void RequestHandler::code200(HttpResponse& response,
+const string& rfctime,const string& path){
+	response.StatusCode = 200;
+	response.addField(string("Cache-Control:public"));
+	response.addField(string("Last-Modified:") + rfctime);
+	Resource resource = ResourceReader::getResource(path);
+	response.setContent(resource.getResouce());
+	response.setContentType(resource.getType());
+}
+
+void RequestHandler::code304(HttpResponse& response){
+	response.StatusCode = 304;
+}
+
+void RequestHandler::code404(HttpResponse& response,
+const string& path){
+	response.StatusCode = 404;
+	Resource resource = ResourceReader::getResource(path);
+	response.setContent(resource.getResouce());
+	response.setContentType(resource.getType());
 }
--- server/RequestHandler.h
+++ server/RequestHandler.h
@@ -4,12 +4,19 @@
 #include "../network/socket.h"
 #include "../network/HttpRequest.h"
 #include "../utility/ByteBuffer.h"
+#include "../network/HttpResponse.h"
 class RequestHandler
 {
 public:
     RequestHandler();
 	~RequestHandler();
 	void handle(Connection conn,HttpRequest request);
-	
+private:
+    void code200(HttpResponse& response,
+	const string& rfctime,const string& path);
+
+	void code304(HttpResponse& response);
+
+	void code404(HttpResponse& response,const string& path);
 };
 #endif//__REQUEST_HANDLER_H__
