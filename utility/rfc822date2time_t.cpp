63628421d6a3397b0295f054d5697a668a4b60c7
liruixin <545976176@qq.com>
2016-11-01 20:02:11 +0800

    fixed 304 add chrome date format

--- utility/rfc822date2time_t.cpp
+++ utility/rfc822date2time_t.cpp
@@ -27,60 +27,124 @@ time_t rfc822date2time_t(string rfc822date){
 	ss >> str;//ignore the day of weekdays 
 	ss >> str;
 
-	btime.tm_mday = stoi(str);
+	if(str[0] >= 48 && str[0] <= 57)
+	{
+		//rfc822
+		btime.tm_mday = stoi(str);
+
+		
+		ss >> str;
+		btime.tm_mon = month2int.at(str) - 1;
+
+		
+		ss >> str;
+		btime.tm_year = stoi(str) - 1900;
 
 	
-	ss >> str;
-	btime.tm_mon = month2int.at(str) - 1;
+		ss >> str;
 
+		//hh:mm:ss
+
+		string::iterator it = str.begin();
+		string::iterator end = str.end();
+
+		string h;
+
+		for(;it != end;it++){
+			if(*it == ':'){
+				it++;
+				break;
+			}else{
+				h.push_back(*it);
+			}
+		}
+
+		btime.tm_hour = stoi(h);
+
+
+		string m;
+
+		for(;it != end;it++){
+			if(*it == ':'){
+				it++;
+				break;
+			}else{
+				m.push_back(*it);
+			}
+		}
+
+		btime.tm_min = stoi(m);
+
+
+		string s;
+
+		for(;it != end;it++){
+			s.push_back(*it);
+		}
+
+		btime.tm_sec = stoi(s);
+		btime.tm_isdst = 0;
+
+		return mktime(&btime);
+	}
+	else{
+		//chrome
+
+		
+		btime.tm_mon = month2int.at(str) - 1;
+
+		ss >> str;
+
+		//hh:mm:ss
+
+		string::iterator it = str.begin();
+		string::iterator end = str.end();
+
+		string h;
+
+		for(;it != end;it++){
+			if(*it == ':'){
+				it++;
+				break;
+			}else{
+				h.push_back(*it);
+			}
+		}
+
+		btime.tm_hour = stoi(h);
+
+
+		string m;
+
+		for(;it != end;it++){
+			if(*it == ':'){
+				it++;
+				break;
+			}else{
+				m.push_back(*it);
+			}
+		}
+
+		btime.tm_min = stoi(m);
+
+
+		string s;
+
+		for(;it != end;it++){
+			s.push_back(*it);
+		}
+
+		btime.tm_sec = stoi(s);
+
+		ss >> str;
+		btime.tm_year = stoi(str) - 1900;
+
+        ss >> str;
+		btime.tm_mday = stoi(str);
+
+		btime.tm_isdst = 0;
+
+		return mktime(&btime);
+	}
 	
-	ss >> str;
-	btime.tm_year = stoi(str) - 1900;
-
-   
-	ss >> str;
-
-	//hh:mm:ss
-
-	string::iterator it = str.begin();
-	string::iterator end = str.end();
-
-    string h;
-
-	for(;it != end;it++){
-		if(*it == ':'){
-			it++;
-			break;
-		}else{
-			h.push_back(*it);
-		}
-	}
-
-	btime.tm_hour = stoi(h);
-
-
-	string m;
-
-	for(;it != end;it++){
-		if(*it == ':'){
-			it++;
-			break;
-		}else{
-			m.push_back(*it);
-		}
-	}
-
-	btime.tm_min = stoi(m);
-
-
-	string s;
-
-	for(;it != end;it++){
-		s.push_back(*it);
-	}
-
-	btime.tm_sec = stoi(s);
-	btime.tm_isdst = 0;
-
-	return mktime(&btime);
 }
